9a0aa3ce8ed04868061bdd4668254f45ca4041a3
denforplay <denforplay@mail.ru>
2021-10-25 13:57:35 +0300

    Override ToString(), GetHashCode(), Equals for vector2

--- ChessLib/ChessLib/Models/Vector2.cs
+++ ChessLib/ChessLib/Models/Vector2.cs
@@ -1,10 +1,12 @@
-﻿namespace ChessLib.Models
+﻿using System;
+
+namespace ChessLib.Models
 {
     /// <summary>
     /// Method provides general vector2
     /// </summary>
     /// <typeparam name="T">Type of vector2</typeparam>
-    public sealed class Vector2<T> where T : struct
+    public sealed class Vector2<T> where T : struct, IComparable
     {
         /// <summary>
         /// Horizontal position
@@ -25,6 +27,41 @@
         {
             X = x;
             Y = y;
+        }
+
+        /// <summary>
+        /// Check if this vector2 equals to other vector2
+        /// </summary>
+        /// <param name="obj">Object to compare</param>
+        /// <returns>True if vectors are equals, other return false</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Vector2<T> vector)
+            {
+                return Y.CompareTo(vector.Y) == 0 && X.CompareTo(vector.X) == 0;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Hash code algorithm for vector2
+        /// </summary>
+        /// <returns>Hash code of vector2</returns>
+        public override int GetHashCode()
+        {
+            int hash = 6688;
+            hash += 12 * (X.GetHashCode() + Y.GetHashCode());
+            return hash;
+        }
+
+        /// <summary>
+        /// Returns a string that represents vector2.
+        /// </summary>
+        /// <returns>A string that represents vector2</returns>
+        public override string ToString()
+        {
+            return $"Vector with coordinates: X{X}, Y{Y}";
         }
     }
 }
